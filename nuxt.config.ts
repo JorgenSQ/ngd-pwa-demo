93328e584c6781202bcf4729cd2c15056ea7d170
lavransbjerkestrand <s341825@oslomet.no>
2021-12-22 00:57:54 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css'
+    ],
+    buildModules: [
+        '@vueuse/nuxt',
+        '@unocss/nuxt'
+    ]
+})
